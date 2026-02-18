7826c97917be91b183d744ab331999a94aba4013
Lucas Calegario <lucas.c.calegario@outlook.com>
2026-02-18 20:03:56 -0300

    fix: add error handling for memory allocation in production initialization macros

--- include/ccabral/_auxds.h
+++ include/ccabral/_auxds.h
@@ -49,4 +49,53 @@ static inline void destroyFirstFollow(FirstFollowEntry **firstFollow)
 CCB_production_t **buildPrdcPrsnTbl(ProductionData **productions);
 void destroyPrdtPrsnTable(CCB_production_t **prdtPrsnTable);
 
+#define INITIALIZE_PRODUCTION_RIGHT_HAND(grammarId, grammarType, production)                            \
+    do                                                                                                  \
+    {                                                                                                   \
+        GrammarData *firstGrammarData = malloc(sizeof(GrammarData));                                    \
+        if (firstGrammarData == NULL)                                                                   \
+        {                                                                                               \
+            fprintf(stderr, "Failed to allocate memory for GrammarData\n");                             \
+            free(production);                                                                           \
+            return NULL;                                                                                \
+        }                                                                                               \
+        firstGrammarData->id = grammarId;                                                               \
+        firstGrammarData->type = grammarType;                                                           \
+        production->rightHandHead = DoublyLinkedListNode__new(firstGrammarData, sizeof(GrammarData *)); \
+        if (production->rightHandHead == NULL)                                                          \
+        {                                                                                               \
+            fprintf(stderr, "Failed to create right hand doubly linked list for start rule 1");         \
+            free(firstGrammarData);                                                                     \
+            free(production);                                                                           \
+            return NULL;                                                                                \
+        }                                                                                               \
+        production->rightHandTail = production->rightHandHead;                                          \
+    } while (0);
+
+#define INSERT_GRAMMAR_TO_PRODUCTION(grammarId, grammarType, production)                        \
+    do                                                                                          \
+    {                                                                                           \
+        GrammarData *grammarData = malloc(sizeof(GrammarData));                                 \
+        if (grammarData == NULL)                                                                \
+        {                                                                                       \
+            fprintf(stderr, "Failed to allocate memory for GrammarData\n");                     \
+            DoublyLinkedListNode__del(production->rightHandHead);                               \
+            free(production);                                                                   \
+            return NULL;                                                                        \
+        }                                                                                       \
+        grammarData->id = grammarId;                                                            \
+        grammarData->type = grammarType;                                                        \
+        if (DoublyLinkedListNode__insertAtTail(                                                 \
+                production->rightHandTail,                                                      \
+                grammarData,                                                                    \
+                sizeof(GrammarData *)) == CCB_ERROR)                                            \
+        {                                                                                       \
+            fprintf(stderr, "Failed to insert grammar in the right hand side of start rule 1"); \
+            DoublyLinkedListNode__del(production->rightHandHead);                               \
+            free(production);                                                                   \
+            return NULL;                                                                        \
+        }                                                                                       \
+        production->rightHandTail = production->rightHandTail->next;                            \
+    } while (0);
+
 #endif
